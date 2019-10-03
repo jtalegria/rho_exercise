4adc2b4bf38f3773d9bd03e215c8e8033f72c1c4
João Alegria <joaotalegria@ua.pt>
2019-10-03 15:15:57 +0100

    [backend] GetOutcomesByEventID Endpoint

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -4,11 +4,13 @@ from django.urls import path, re_path
 
 from sports.views import (
                             GetAllSports, 
-                            GetAllEventsBySportID
+                            GetAllEventsBySportID,
+                            GetOutcomesByEventID
                         )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^sports/$', GetAllSports.as_view(), name='Sports-Views'),
     url(r'^sports/(?P<sport_id>\d+)/$', GetAllEventsBySportID.as_view(), name='Sports-Views'),
+    url(r'^sports/(?P<sport_id>\d+)/events/(?P<event_id>\d+)$', GetOutcomesByEventID.as_view(), name='Sports-Views'),
 ]
--- backend/api/sports/views.py
+++ backend/api/sports/views.py
@@ -34,3 +34,38 @@ class GetAllEventsBySportID(GenericAPIView):
                             list_events.append(comp[i]['events'][j])
                 return Response(status=status.HTTP_200_OK, data=list_events)
         return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "Invalid Sport ID"})
+
+class GetOutcomesByEventID(GenericAPIView):
+    def get(self, request, sport_id, event_id):
+        sport_id = int(sport_id)
+        event_id = int(event_id)
+
+        for i in range(len(ordered_sports)):
+            if ordered_sports[i]['id'] == sport_id:
+                comp = ordered_sports[i]['comp']
+                if not comp:
+                    return Response(status=status.HTTP_400_BAD_REQUEST, data={"warning": "There is no events for this Sport"})
+
+                list_events = []
+                for i in range (0, len(comp)):
+                    if len(comp[i]['events']) == 1:
+                        list_events.append(comp[i]['events'][0])
+                    else:
+                        for j in range(0, len(comp[i]['events'])):
+                            list_events.append(comp[i]['events'][j])
+
+                for i in range(0, len(list_events)):
+                    if list_events[i]['id'] == event_id:
+                        teamA = list_events[i]['oppADesc']
+                        teamB = list_events[i]['oppBDesc']
+                        final_scr = list_events[i]['scoreboard']['scr']
+                        final_scr_parts = final_scr.split("-")
+                        return Response(status=status.HTTP_200_OK, data={"outcome": final_scr, 
+                                                                        "details": {
+                                                                            teamA: int(final_scr_parts[0]),
+                                                                            teamB: int(final_scr_parts[1])
+                                                                            }
+                                                                        })
+                    else:
+                        return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "Invalid Event ID"})
+
