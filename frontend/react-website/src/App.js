32953b148a5d1b2805813002bfe6bde7a54893b1
João Alegria <joaotalegria@ua.pt>
2019-10-14 11:51:22 +0100

    [frontend] React Router Implementation

--- frontend/react-website/src/App.js
+++ frontend/react-website/src/App.js
@@ -1,31 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Home from "./components/Home"
+import Sport from "./components/Sport"
+import Page404 from "./components/Page404"
 
 function App(){
   return (
-    "ola"
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component = {Home} />
+          <Route exact path="/events/:sportID(\d+)" component = {Sport} name="sport"/>
+          <Route component = {Page404} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
